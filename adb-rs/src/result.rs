2b6d9ada0f2bf423c1ceb97a26b4c03e1f49a9d5
David Craven <david@craven.ch>
2022-04-26 22:33:27 +0200

    Run rustfmt.

--- adb-rs/src/result.rs
+++ adb-rs/src/result.rs
@@ -2,47 +2,47 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum AdbError {
-  #[error("io error: {}", _0)]
-  Io(std::io::Error),
+    #[error("io error: {}", _0)]
+    Io(std::io::Error),
 
-  #[error("data crc mismatch")]
-  Crc,
+    #[error("data crc mismatch")]
+    Crc,
 
-  #[error("auth not supported")]
-  AuthNotSupported,
+    #[error("auth not supported")]
+    AuthNotSupported,
 
-  #[error("unknown command: {:x}", _0)]
-  UnknownCommand(u32),
+    #[error("unknown command: {:x}", _0)]
+    UnknownCommand(u32),
 
-  #[error("unexpected command: {:?}", _0)]
-  UnexpectedCommand(crate::message::Command),
+    #[error("unexpected command: {:?}", _0)]
+    UnexpectedCommand(crate::message::Command),
 
-  #[error("unexpected data: {:?}", _0)]
-  UnexpectedData(Vec<u8>),
+    #[error("unexpected data: {:?}", _0)]
+    UnexpectedData(Vec<u8>),
 
-  #[error("disconnected")]
-  Disconnected,
+    #[error("disconnected")]
+    Disconnected,
 
-  #[error("fail: {}", _0)]
-  Fail(String),
+    #[error("fail: {}", _0)]
+    Fail(String),
 }
 
 impl AdbError {
-  pub fn from_unexpected_command_u32(cmd: u32) -> Self {
-    use crate::message::Command;
-    use num_traits::FromPrimitive;
-    if let Some(cmd) = Command::from_u32(cmd) {
-      AdbError::UnexpectedCommand(cmd)
-    } else {
-      AdbError::UnknownCommand(cmd)
+    pub fn from_unexpected_command_u32(cmd: u32) -> Self {
+        use crate::message::Command;
+        use num_traits::FromPrimitive;
+        if let Some(cmd) = Command::from_u32(cmd) {
+            AdbError::UnexpectedCommand(cmd)
+        } else {
+            AdbError::UnknownCommand(cmd)
+        }
     }
-  }
 }
 
 pub type AdbResult<T> = Result<T, AdbError>;
 
 impl From<::std::io::Error> for AdbError {
-  fn from(err: ::std::io::Error) -> AdbError {
-    AdbError::Io(err)
-  }
+    fn from(err: ::std::io::Error) -> AdbError {
+        AdbError::Io(err)
+    }
 }
--- adb-rs/src/shell.rs
+++ adb-rs/src/shell.rs
@@ -4,28 +4,28 @@ use super::client::*;
 use crate::result::*;
 
 pub trait AdbShell {
-  fn shell_exec(&mut self, cmd: &str) -> AdbResult<Vec<u8>>;
+    fn shell_exec(&mut self, cmd: &str) -> AdbResult<Vec<u8>>;
 }
 
 impl AdbShell for AdbConnection {
-  fn shell_exec(&mut self, cmd: &str) -> AdbResult<Vec<u8>> {
-    let stream = self.open_stream(&format!("shell:{}", cmd))?;
-    let mut buf = vec![];
+    fn shell_exec(&mut self, cmd: &str) -> AdbResult<Vec<u8>> {
+        let stream = self.open_stream(&format!("shell:{}", cmd))?;
+        let mut buf = vec![];
 
-    loop {
-      let packet = stream.recv()?;
-      match packet.command {
-        Command::A_WRTE => {
-          buf.write_all(&packet.payload)?;
-          stream.send_ok()?;
+        loop {
+            let packet = stream.recv()?;
+            match packet.command {
+                Command::A_WRTE => {
+                    buf.write_all(&packet.payload)?;
+                    stream.send_ok()?;
+                }
+                Command::A_CLSE => {
+                    break;
+                }
+                cmd => return Err(AdbError::UnexpectedCommand(cmd)),
+            }
         }
-        Command::A_CLSE => {
-          break;
-        }
-        cmd => return Err(AdbError::UnexpectedCommand(cmd)),
-      }
+
+        Ok(buf)
     }
-
-    Ok(buf)
-  }
 }
